fe4993e0a8f9391b4463199371e7b38211073b95
masayan382 <masan3821031@gmail.com>
2022-09-20 23:14:42 +0900

    input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Avatar from "../components/Avatar"
 import { MicrophoneIcon } from "@heroicons/react/24/solid"
+import Image from "next/image"
 
 export default function Home() {
     return (
@@ -41,6 +42,44 @@ export default function Home() {
                 </div>
             </header>
             {/* body */}
+            <form action="">
+                <Image
+                    src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                    height={100}
+                    width={300}
+                />
+                <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md ">
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-5 h-5 mr-3 text-gray-500"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <input
+                        type="text"
+                        className="focus:outline-none flex-grow"
+                    />
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-5 h-5"
+                        >
+                            <path d="M8.25 4.5a3.75 3.75 0 117.5 0v8.25a3.75 3.75 0 11-7.5 0V4.5z" />
+                            <path d="M6 10.5a.75.75 0 01.75.75v1.5a5.25 5.25 0 1010.5 0v-1.5a.75.75 0 011.5 0v1.5a6.751 6.751 0 01-6 6.709v2.291h3a.75.75 0 010 1.5h-7.5a.75.75 0 010-1.5h3v-2.291a6.751 6.751 0 01-6-6.709v-1.5A.75.75 0 016 10.5z" />
+                        </svg>
+                    </div>
+                </div>
+            </form>
             {/* footer */}
         </div>
     )
