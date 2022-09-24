0879a58ab6be6e3e1662dad8208b80410a870641
masayan382 <masan3821031@gmail.com>
2022-09-24 11:53:53 +0900

    page

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -4,7 +4,59 @@ import Link from "next/link"
 const PaginationButtons = () => {
     const router = useRouter()
     const startIndex = Number(router.query.start) || 0
-    return <div></div>
+    return (
+        <div className="flex justify-between max-w-lg text-blue-700 mb-10">
+            {startIndex >= 10 && (
+                <Link
+                    href={`/search?term=${router.query.term}&start=${
+                        startIndex - 10
+                    } `}
+                >
+                    <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-5 h-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15.75 19.5L8.25 12l7.5-7.5"
+                            />
+                        </svg>
+
+                        <p>Previous</p>
+                    </div>
+                </Link>
+            )}
+            <Link
+                href={`/search?term=${router.query.term}&start=${
+                    startIndex + 10
+                } `}
+            >
+                <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                        />
+                    </svg>
+                    <p>Next</p>
+                </div>
+            </Link>
+        </div>
+    )
 }
 
 export default PaginationButtons
